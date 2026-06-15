29dcc0b84fb03f57c01dc0a4bde895da5cba1e6f
nairodx77-star <nairodx77@gmail.com>
2026-06-15 15:48:24 +0200

    Mount payment tracker below housing contribution

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
+import { initPaymentTracker } from "./paymentTracker.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+initPaymentTracker();
